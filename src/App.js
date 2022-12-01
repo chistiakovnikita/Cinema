19ce06a70182c1437c55470c0d6f0340d5c2280e
Nikita Chistiakov <nikita_chnv@mail.ru>
2022-12-01 21:39:19 +0300

    cinema

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import { Component } from "./core";
-import './components/atoms/typography/Headings/H1'
+import './components'
+import { movieService } from "./services/MovieService";
+
 
 export class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      searchValue: '',
+      selectedCategory: ''
+    }
+  }
+
+  componentDidMount() {
+    movieService.getAllMovies()
+    .then(({ data }) => {
+      this.setState((state) => {
+        return {
+          ...state,
+          movies: data
+        }
+      })
+    })
+  }
+
   render() {
+
     return ( 
       `
-        <it-h1>
-          <div>Hello</div>
-        </it-h1>
+       <div id="shell">
+        <it-header></it-header>
+        <movie-card></movie-card>
+       </div>
       `
       
     )
